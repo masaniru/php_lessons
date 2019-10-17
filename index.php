fd39bf0d7b8a0d6b2ef4b91b52a801dceaa9b329
EC2 Default User <ec2-user@ip-172-31-37-128.ap-northeast-1.compute.internal>
2019-10-17 15:46:16 +0000

    コミットテスト

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+define("MY_EMAIL","masaniru@gmail.com");
+$msg = "Hello World from PHP!";
+
+echo MY_EMAIL;
+
+echo $msg;
+
+var_dump($msg);
+var_dump(__LINE__);
+var_dump(__FILE__);
+var_dump(__DIR__);
